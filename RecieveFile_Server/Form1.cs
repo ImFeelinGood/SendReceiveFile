e18c6b8e3ace6ffc908be1227d5d28debf695b7f
Daffa Yahya <daffayahya2003@gmail.com>
2024-09-24 18:25:54 +0700

    SendRecieveFile
    18_Daffa

---
+++ RecieveFile_Server/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace RecieveFile_Server
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+        private ArrayList alSockets;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ServerForm_Load(object sender, EventArgs e)
+        {
+            IPHostEntry IPHost = Dns.GetHostEntry(Dns.GetHostName());
+            lblStatus.Text = "Server IP Address: " + IPHost.AddressList[0].ToString();
+
+            alSockets = new ArrayList();
+
+            Thread thdListener = new Thread(new ThreadStart(listenerThread));
+            thdListener.Start();
+        }
+
+        private void UpdateListBox(string message)
+        {
+            if (lbConnections.InvokeRequired)
+            {
+                lbConnections.Invoke(new MethodInvoker(() => UpdateListBox(message)));
+            }
+            else
+            {
+                lbConnections.Items.Add(message);
+            }
+        }
+
+        private void listenerThread()
+        {
+            TcpListener tcpListener = new TcpListener(IPAddress.Any, 8080);
+            tcpListener.Start();
+            while (true)
+            {
+                Socket handlerSocket = tcpListener.AcceptSocket();
+                if (handlerSocket.Connected)
+                {
+                    UpdateListBox(handlerSocket.RemoteEndPoint.ToString() + " connected.");
+                    lock (this)
+                    {
+                        alSockets.Add(handlerSocket);
+                    }
+                    Thread thdHandler = new Thread(handlerThread);
+                    thdHandler.Start();
+                }
+            }
+        }
+
+        public void handlerThread()
+        {
+            Socket handlerSocket = (Socket)alSockets[alSockets.Count - 1];
+            NetworkStream networkStream = new NetworkStream(handlerSocket);
+            int thisRead = 0;
+            int blockSize = 1024;
+            Byte[] dataByte = new Byte[blockSize];
+
+            try
+            {
+                using (Stream fileStream = File.OpenWrite("C:/Users/dheda/Documents/test.txt"))
+                {
+                    while (true)
+                    {
+                        thisRead = networkStream.Read(dataByte, 0, blockSize);
+                        if (thisRead == 0) break;
+                        fileStream.Write(dataByte, 0, thisRead);
+                    }
+                }
+
+                UpdateListBox("File Written");
+            }
+            catch (Exception ex)
+            {
+                UpdateListBox("Error: " + ex.Message);
+            }
+            finally
+            {
+                handlerSocket.Close();
+                handlerSocket = null;
+            }
+        }
+
+    }
+}
---
+++ SendFile_Client/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace SendFile_Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.tbFilename = new System.Windows.Forms.TextBox();
+            this.tbServer = new System.Windows.Forms.TextBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(301, 46);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(301, 12);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // tbFilename
+            // 
+            this.tbFilename.Location = new System.Drawing.Point(71, 12);
+            this.tbFilename.Name = "tbFilename";
+            this.tbFilename.Size = new System.Drawing.Size(220, 20);
+            this.tbFilename.TabIndex = 2;
+            // 
+            // tbServer
+            // 
+            this.tbServer.Location = new System.Drawing.Point(71, 52);
+            this.tbServer.Name = "tbServer";
+            this.tbServer.Size = new System.Drawing.Size(220, 20);
+            this.tbServer.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "File Path:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Server IP:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 84);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbServer);
+            this.Controls.Add(this.tbFilename);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.btnSend);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "18_Daffa SendFile_Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            openFileDialog.ShowDialog();
+            tbFilename.Text = openFileDialog.FileName;
+        }
+
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            Stream fileStream = File.OpenRead(tbFilename.Text);
+            byte[] fileBuffer = new byte[fileStream.Length];
+            fileStream.Read(fileBuffer, 0, (int)fileStream.Length);
+            TcpClient clientSocket = new TcpClient(tbServer.Text, 8080);
+            NetworkStream networkStream = clientSocket.GetStream();
+            networkStream.Write(fileBuffer, 0, fileBuffer.Length);
+            networkStream.Close();
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox tbFilename;
+        private System.Windows.Forms.TextBox tbServer;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
